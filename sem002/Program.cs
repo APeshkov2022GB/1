a5d5eb315a385a50b533d9d86b2942a573829cf0
Peshkov Aleksandr <pas-0787@mail.ru>
2022-04-26 00:56:38 +0300

    sem002_1

--- sem002/Program.cs
+++ sem002/Program.cs
@@ -1,20 +1,20 @@
 ﻿//Даны два числа. Показать большее и меньшее число
 Console.WriteLine("Введите первое число ");
-int x = Console.ReadLine();
+int x = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
-int y = Console.ReadLine();
+int y = int.Parse(Console.ReadLine());
 
 if(x > y)
-
+{
     Console.WriteLine("Большее число");
     Console.WriteLine(x);
     Console.WriteLine("Меньшее число");
-    Console.WriteLine(y)
-
+    Console.WriteLine(y);
+}
 else
-
+{
     Console.WriteLine("Большее число");
     Console.WriteLine(y);
     Console.WriteLine("Меньшее число");
     Console.WriteLine(x);
-
+}
