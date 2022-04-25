0cc60bde0ac7ff60004dce495db02524a3477941
Peshkov Aleksandr <pas-0787@mail.ru>
2022-04-26 00:45:21 +0300

    sem001 and sem002

--- sem001/Program.cs
+++ sem001/Program.cs
@@ -5,9 +5,16 @@
 //Console.WriteLine(kvadrat);
 
 
-Console.WriteLine("Введите число ");
-string a = Console.ReadLine();
-int x = int.Parse(a);
-int kvadrat = x*x;
+//Console.WriteLine("Введите число ");
+//string a = Console.ReadLine();
+//int x = int.Parse(a);
+//int kvadrat = x*x;
+//Console.Write("Квадрат числа = ");
+//Console.WriteLine(kvadrat);
+
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int kvadrat = a * a;
 Console.Write("Квадрат числа = ");
 Console.WriteLine(kvadrat);
+
---
+++ sem002/Program.cs
@@ -0,0 +1,20 @@
+﻿//Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Введите первое число ");
+int x = Console.ReadLine();
+Console.WriteLine("Введите второе число ");
+int y = Console.ReadLine();
+
+if(x > y)
+
+    Console.WriteLine("Большее число");
+    Console.WriteLine(x);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(y)
+
+else
+
+    Console.WriteLine("Большее число");
+    Console.WriteLine(y);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(x);
+
