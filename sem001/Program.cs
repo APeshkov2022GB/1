23034043775d50df6495f83438c715be867eec57
Peshkov Aleksandr <pas-0787@mail.ru>
2022-04-26 00:03:22 +0300

    sem001

---
+++ sem001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Семинар Задача 1 Вывести квадрат числа
+//int a = 10;
+//int kvadrat = a*a;
+//Console.Write(" Квадрат числа = ");
+//Console.WriteLine(kvadrat);
+
+
+Console.WriteLine("Введите число ");
+string a = Console.ReadLine();
+int x = int.Parse(a);
+int kvadrat = x*x;
+Console.Write(" Квадрат числа = ");
+Console.WriteLine(kvadrat);
