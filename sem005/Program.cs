8bd5e1a1609d73f664a5b745c0d3ba9deaeee7aa
Peshkov Aleksandr <pas-0787@mail.ru>
2022-04-29 22:18:54 +0300

    sem005_3

--- sem005/Program.cs
+++ sem005/Program.cs
@@ -1,5 +1,13 @@
 ﻿//Показать последнюю  цыфру трёхзначного числа.
 Console.WriteLine("Введите число ");
 int x = int.Parse(Console.ReadLine());
+if (x > 99 && x < 1000)  // проверка что число трёхзное
+{
 Console.Write("Последняя цыфра 3-х значного числа = ");
 Console.WriteLine(x % 10);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
+Console.WriteLine("Программа работу закончила!!!"); 
