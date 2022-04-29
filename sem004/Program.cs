8757ced6cdef6cbac0891e55851486f3fd783d39
Peshkov Aleksandr <pas-0787@mail.ru>
2022-04-29 21:40:23 +0300

    sem003 sem 004

---
+++ sem004/Program.cs
@@ -0,0 +1,15 @@
+﻿// показать числа от -N до N
+Console.WriteLine("Введите число ");
+int x = int.Parse(Console.ReadLine());
+
+int min = -x;
+
+while(min <= x)
+{
+    //Console.WriteLine(" " + min);  или // console.WriteLine(min); столбиком виведет значения
+    Console.Write(" " + min);
+    //min = min +1; min += 1;
+    min++;
+}
+Console.WriteLine(" ");    
+Console.WriteLine("Программа работу закончила!!!");    
