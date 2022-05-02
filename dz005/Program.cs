2b882374bd8561f4dac0f6429584ee0ba5879d1b
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-02 17:55:17 +0300

    add dz 005

---
+++ dz005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число ");
+int x = int.Parse(Console.ReadLine());
+if (x > 99 && x < 1000)  // проверка что число трёхзное
+{
+    Console.Write("Вторая цыфра 3-х значного числа = ");
+    Console.WriteLine(x / 10 % 10);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
+Console.WriteLine("Программа работу закончила!!!");
