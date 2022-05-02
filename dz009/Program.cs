41556e5ddb3d8af410e5991e37ca764f6a3c47bb
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-02 22:40:23 +0300

    add dz009

---
+++ dz009/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Ведите число до 9999");
+int namber = int.Parse(Console.ReadLine());
+Console.WriteLine($"Дано число: {namber}");
+if (namber / 100 > 0) 
+    Console.WriteLine($" третьей цыфрой числа {namber} является цыфра {namber / 10 % 10}");
+else 
+    Console.WriteLine($"У числа {namber} нет третьей цыфры");
+Console.WriteLine("Программа работу закончила!!!");  
