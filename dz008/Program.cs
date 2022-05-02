34792863e51ef2c6d2d1c2953e10a7dd6ddf4273
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-02 22:13:57 +0300

    add dz008

---
+++ dz008/Program.cs
@@ -0,0 +1,25 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число для проверки кратности");
+int kratno = int.Parse(Console.ReadLine());
+
+if (number % kratno == 0) 
+{
+    Console.WriteLine($"Число {number} кратно {kratno} и делится без остатка");
+}
+else 
+{
+    int ost = number % kratno;
+    Console.WriteLine("Остаток от деления = " + ost);
+}
+Console.WriteLine("Программа работу закончила!!!");
+
+
+
+
+
+
+
+
+
