c4b6877b2376d2dce5bf8d0c89be22bee960c717
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-02 14:50:34 +0300

    add dz003

---
+++ dz003/Program.cs
@@ -0,0 +1,12 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0) 
+{
+    Console.WriteLine("Введенное число является четным");
+}
+else
+{
+    Console.WriteLine("Введенное число не является четным");
+}
+Console.WriteLine("Программа работу закончила!!!");
