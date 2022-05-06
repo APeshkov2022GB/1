e94ab213d537c3dfc8dc42a821becbdfbe28c5b3
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-06 20:48:35 +0300

    add dz 06 08 09

--- dz008/Program.cs
+++ dz008/Program.cs
@@ -3,7 +3,6 @@ Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число для проверки кратности");
 int kratno = int.Parse(Console.ReadLine());
-
 if (number % kratno == 0) 
 {
     Console.WriteLine($"Число {number} кратно {kratno} и делится без остатка");
@@ -16,7 +15,19 @@ else
 Console.WriteLine("Программа работу закончила!!!");
 
 
-
+// // Выяснить, кратно ли число заданному, если нет, вывести остаток.
+// Console.WriteLine("Введите первое число: ");
+// int number1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = int.Parse(Console.ReadLine());
+// if (number1%number2 == 0) Console.WriteLine($"Число {number1} кратно числу {number2} ");
+// else if (number2%number1 == 0) Console.WriteLine($"Число {number2} кратно числу {number1} ");
+// else 
+// {
+//     Console.Write($"Числа {number1} и {number2} не кратны, ");
+//     if (number1>number2) Console.WriteLine($"остаток от деления {number1} на {number2} = {number1%number2}. ");
+//     else Console.WriteLine($"остаток от деления {number2} на {number1} = {number2%number1}. ");
+// }
 
 
 
--- dz009/Program.cs
+++ dz009/Program.cs
@@ -7,3 +7,18 @@ if (namber / 100 > 0)
 else 
     Console.WriteLine($"У числа {namber} нет третьей цыфры");
 Console.WriteLine("Программа работу закончила!!!");  
+
+
+// // Найти третью цифру числа или сообщить, что её нет
+// Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine()?? "0");
+// if (n > 99 && n < 1000 || n > -1000 && n < -99)
+// Console.WriteLine($"Третья цифра трёхзначного числа = {n%10} ");
+// else if (n > -100 && n < 100)
+// Console.WriteLine("Введено число, в котором нет третьей цифры! ");
+// else
+// {
+//     while (n > 999 || n < -999)
+//     n=n/10;
+// Console.WriteLine($"Третья цифра четырёхзначного или большего числа = {Math.Abs(n%10)} ");
+// }
