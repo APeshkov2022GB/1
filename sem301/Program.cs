3aff0e3de6de8661cf2be6bf2cfda26acd0837e8
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-18 20:57:19 +0300

    add sem301

---
+++ sem301/Program.cs
@@ -0,0 +1,20 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine() ?? "");
+int i=1;
+
+while (i <= n)
+  {
+    Console.Write(i*i + ", ");
+    i = i + 1;
+  }  
+Console.WriteLine("Программа работу закончила");
+
+
+// / Показать таблицу квадратов чисел от 1 до N
+// Console.WriteLine("Введите число n: ");
+// int n = int.Parse(Console.ReadLine());
+// for (int i = 1; i <=n; i++)
+// {
+// Console.Write($"{i * i} ");
+// }
