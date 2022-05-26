e7da3676bda33ecaebb7c015305c08967ac4bb74
Peshkov Aleksandr <pas-0787@mail.ru>
2022-05-26 22:11:49 +0300

    add all

---
+++ Exs401/Program.cs
@@ -0,0 +1,65 @@
+﻿// двухерные массивы в програмировании. Лекция
+/*
+void PrintArray(int [,] matr)
+{ 
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+        
+    }         
+        
+}
+
+int [,] matrix = new int[3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+*/
+
+//----------------------------------------------
+//Факториал
+/*
+double Factorial(double n)
+{
+    if (n == 1 ) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+*/
+//---------------------------------------------------------
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($" f({i}) = {Fibonacci(i)}");
+}
+
+
+
+
+
+
--- sem601/Program.cs
+++ sem601/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+/*
+Console.Clear();
+Console.Write("Введите кол-во строк двухмерного массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов двухмерного массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] arr = new int[m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i,j] = new Random().Next(-10,11);
+        Console.Write($"{arr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
+*/
+// Показать двумерный массив размером m×n заполненный целыми числами
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+Console.WriteLine("Введите число m, n: ");
+int m = int.Parse(Console.ReadLine()??"0");
+int n = int.Parse(Console.ReadLine()??"0");
+
+int[,] matrix = new int [m,n];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
