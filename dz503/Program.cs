f1971e42c9f8ccb6f3160f787ccba3a629b4f3e6
Aleksandr Peshkov <pas-0787@mail.ru>
2022-06-09 00:21:26 +0300

    add dz503

---
+++ dz503/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу копирования массива
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+
+ 
+Console.WriteLine("Введите число m, n: ");
+int m = int.Parse(Console.ReadLine()??"0");
+int n = int.Parse(Console.ReadLine()??"0");
+
+int[,] matrix = new int [m,n];
+int[,] matrix1 = new int [m,n];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+for (int i =0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           matrix1[i,j] = matrix[i,j]; 
+        }  
+    }
+PrintArray(matrix1);
