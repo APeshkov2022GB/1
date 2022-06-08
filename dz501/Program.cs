57f64bcfd8f2ac0353269f42a7b70dd795b04b36
Aleksandr Peshkov <pas-0787@mail.ru>
2022-06-08 22:56:15 +0300

    add all

---
+++ dz501/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Найти точку пересечения двух прямых
+// заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.Clear();
+Console.WriteLine("Введите b1");
+int b1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите k1");
+int k1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите b2");
+int b2 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите k2");
+int k2 = int.Parse(Console.ReadLine() ?? "");
+if (k1 == k2)
+    Console.WriteLine(" Прямые параллельны, точек переcечения нет");
+else
+{
+    int x = (b2 - b1) / (k1 - k2);
+    int y = k2 * x + b2;
+    Console.WriteLine($"Прямые пересекаются в точке с координатой х= {x},y = {y}");
+}
+Console.WriteLine("Программа работу закончила!!! ");
---
+++ dz502/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Написать программу масштабирования фигуры
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] mas)// ввод координат x и y в двумерный массив
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            if (j == 0)
+            {
+                Console.WriteLine($" Введите Х{i + 1}");
+                mas[i, j] = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine($" Введите Y{i + 1}");
+                mas[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во вершин фигуры: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] mas = new int[n, 2];
+FillArray(mas);
+PrintArray(mas);
+
+Console.WriteLine(" Введите коэффициент масштабирования k = ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+int[,] mas1 = new int[n, 2];
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        mas1[i, j] = mas[i, j] * k;
+    }
+}
+PrintArray(mas1);
+Console.WriteLine("Программа работу закончила");
+
